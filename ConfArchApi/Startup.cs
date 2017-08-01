c79500c69a9ea5d133b7e016eb4dcf9d2f24fa8d
Jose Benavides <jbenavides.bermudez@gmail.com>
2017-08-01 10:17:12 -0500

    Adding code to basic configuration of Identity server

---
+++ ConfArchApi/Startup.cs
@@ -0,0 +1,45 @@
+﻿using ConfArchApi.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConfArchApi
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSingleton<AttendeeRepo>();
+            services.AddSingleton<ConferenceRepo>();
+            services.AddSingleton<ProposalRepo>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            /*app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
+            {
+                Authority = "http://localhost:5000",
+                ApiName = "confArchApi",
+                AllowedScopes = new[] { "confArchApi" },
+
+                RequireHttpsMetadata = false
+            });*/
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ ConfArchWeb/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using ConfArchWeb.Api;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConfArchWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<ConferenceApiService>();
+            services.AddTransient<ProposalApiService>();
+            services.AddTransient<AttendeeApiService>();
+            services.AddSingleton(x => new HttpClient {BaseAddress = new Uri("http://localhost:54438")});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, HttpClient httpClient)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseDeveloperExceptionPage();
+            /*
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Cookies"
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                AuthenticationScheme = "oidc",
+                SignInScheme = "Cookies",
+
+                Authority = "http://localhost:5000",
+                RequireHttpsMetadata = false,
+
+                ClientId = "confarchweb",
+                ClientSecret = "secret",
+
+                ResponseType = "code id_token",
+                Scope = { "confArchApi" },
+                SaveTokens = true       
+            });*/
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Conference}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
